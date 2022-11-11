1e3eac1c3b5c0bd046a28e14616a5f38aad64a25
NIKIDIGI <nikidigi.yt@gmail.com>
2022-11-11 06:48:25 +0000

    apocriton - assign resurrect mech to the mechanitor

---
+++ Source/Patch_CompAbilityEffect_ResurrectMech.cs
@@ -0,0 +1,37 @@
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace ResurrectEnemyMechanoids
+{
+    [HarmonyPatch(typeof(CompAbilityEffect_ResurrectMech), nameof(CompAbilityEffect_ResurrectMech.Apply))]
+    class Patch_CompAbilityEffect_ResurrectMech_Apply
+    {
+        static Pawn mech = null;
+
+        static void Prefix(ref LocalTargetInfo target)
+        {
+            Corpse corpse = (Corpse)target.Thing;
+
+            if (corpse.InnerPawn.IsColonyMech)
+            {
+                mech = corpse.InnerPawn;
+            }
+        }
+
+        static void Postfix(CompAbilityEffect_ResurrectMech __instance, ref LocalTargetInfo target)
+        {
+            if (mech is not null)
+            {
+                Pawn overseer = __instance.parent.pawn.GetOverseer();
+
+                if (overseer is not null && MechanitorUtility.CanControlMech(overseer, mech))
+                {
+                    overseer.relations.AddDirectRelation(PawnRelationDefOf.Overseer, mech);
+                }
+
+                mech = null;
+            }
+        }
+    }
+}
